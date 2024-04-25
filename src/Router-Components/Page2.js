81343ebec06dddf4a30e9ed5169aab7a9b24ded1
hassanprodeveloper <hassanprodeveloper@gmail.com>
2024-04-25 16:24:42 +0500

    Talha work

--- src/Router-Components/Page2.js
+++ src/Router-Components/Page2.js
@@ -1,10 +1,16 @@
 import React from "react";
 import NavbarPage2 from "../components/NavbarPage2/NavbarPage2";
-
+import { Row, Col } from "react-bootstrap";
 const Page2 = () => {
   return (
     <div>
       <NavbarPage2 />
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
     </div>
   );
 };
